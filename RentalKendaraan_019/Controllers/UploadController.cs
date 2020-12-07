b5374e5399356890eaf2bcfd0e05b55fbd596f3b
tomblong <tomblong@users.noreply.github.com>
2020-12-08 00:27:58 +0700

    Perbaikan Modul 9 ntah bener ntah nggak , tombol upload cuman work yang paling bawah

--- RentalKendaraan_019/Controllers/UploadController.cs
+++ RentalKendaraan_019/Controllers/UploadController.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using RentalKendaraan_019.Models.Upload;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.FileProviders;
-using RentalKendaraan_019.Models.Upload;
 
-namespace RentalKendaraan_019.Controllers
+namespace RentalKendaraan_022.Controllers
 {
     public class UploadController : Controller
     {
@@ -126,4 +126,5 @@ namespace RentalKendaraan_019.Controllers
                 };
         }
     }
+
 }
